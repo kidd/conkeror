18ea3f170206fbebc46ea374f4de7c0ab593f1cb
Raimon Grau <raimonster@gmail.com>
2013-06-03 17:12:02 +0200

    history for lisp

--- modules/jscl_loader.js
+++ modules/jscl_loader.js
@@ -7,7 +7,10 @@ globalEval = evaluate;
 interactive("lisp",
             "Evaluate a Lisp expr",
             function (I) {
-                var input = I.minibuffer.read($prompt = "Lisp expression: ");
+                var input = I.minibuffer.read($prompt = "Lisp expression: ",
+                                             $history = "lisp-eval");
                 var result = lisp.evalString.apply(window, [yield input]);
                 I.window.minibuffer.message(lisp.print(result));
             });
+
+provide("jscl_loader")
